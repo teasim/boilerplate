87af5fab51943d618b62fb4bbf40b11f699426d5
Billgo <cocobill@vip.qq.com>
2018-10-03 17:29:25 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = api => {
   api.cache(false);
   return {
     presets: ['habitual'],
+    plugins: [
+      [
+        'babel-plugin-import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'lib',
+          style: true,
+        },
+      ],
+    ],
   };
 };
