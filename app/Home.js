99b2e0af5f55c722002fe05fe79414950f103534
Billgo <cocobill@vip.qq.com>
2018-10-03 16:45:06 +0800

    update

--- app/Home.js
+++ app/Home.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { createSelector } from "reselect";
-import { Button } from "teasim";
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import { Button } from 'teasim';
 
 const Home = props => (
-  <div className="pa-wide" >
-    <div className="mb-wide font-wide">{ props.count }</div>
-    <Button 
-      key="add" 
-      onClick={() => { 
-        props.dispatch({type: 'digit/add',payload:props.count})}
-      }
+  <div className="pa-wide">
+    <div className="mb-wide font-wide">{props.count}</div>
+    <Button
+      key="add"
+      onClick={() => {
+        props.dispatch({ type: 'digit/add', payload: props.count });
+      }}
     >
       +
     </Button>
-    <Button 
-      key="minus" 
-      onClick={() => { 
-        props.dispatch({type: 'digit/minus',payload:props.count})}
-      }
+    <Button
+      key="minus"
+      onClick={() => {
+        props.dispatch({ type: 'digit/minus', payload: props.count });
+      }}
     >
       -
     </Button>
+    <Button skin="blue">Hello World</Button>
   </div>
 );
 
 const selectCount = () =>
-  createSelector(state => state.get("digit"), count => count.get("count"));
+  createSelector(state => state.get('digit'), count => count.get('count'));
 
 const mapStateToProps = createSelector(selectCount(), count => ({
-  count
+  count,
 }));
 
 export default connect(mapStateToProps)(Home);
